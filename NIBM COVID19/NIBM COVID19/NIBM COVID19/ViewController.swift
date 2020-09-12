b0712989f2092bfa54da8d2189a3127d6577870d
Ruvinda Lahiru Samaranayake <androidruvee@gmail.com>
2020-09-12 22:45:13 +0530

    Add files via upload

---
+++ NIBM COVID19/NIBM COVID19/NIBM COVID19/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NIBM COVID19
+//
+//  Created by Ruvinda Lahiru Samaranayake on 9/12/20.
+//  Copyright © 2020 Ruvinda Lahiru Samaranayake. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
